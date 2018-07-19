376bd3a568e55a7c4a67a11ce25c7876d0ed03cd
Velar <Sujal Paudel>
2018-07-19 17:39:23 +0545

    Implemented basic email functionality

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname, '../public');
-const port = Process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
   console.log("New User connected");
+
+  socket.emit('newEmail', {
+    from: 'sujal@example.com',
+    text: 'Hey, whats up?',
+    createdAt: 123
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('createEmail', newEmail);
+  });
 
   socket.on('disconnect', () => {
     console.log("User disconnected");
