c2451c8c807af205d925b4a6cfabd497c13607f1
Velar <thesujal17@gmail.com>
2018-07-24 19:12:53 +0545

    corrected the username missing error

--- public/js/chat.js
+++ public/js/chat.js
@@ -44,6 +44,7 @@ socket.on('newLocationMessage', function(message){
   var formattedTime = moment(message.createdAt).format('h:mm a');
   var template = jQuery('#location-message-template').html();
   var html = Mustache.render(template, {
+    from : message.from,
     url : message.url,
     createdAt : formattedTime
   });
