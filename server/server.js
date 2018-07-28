af7bf7d9f9b1e8fa0af7fcd2498e4abb98ffa601
Velar <thesujal17@gmail.com>
2018-07-28 17:13:46 +0545

    Updated the personal send location

--- server/server.js
+++ server/server.js
@@ -39,12 +39,21 @@ io.on('connection', (socket) =>{
   });
 
   socket.on('createMessage', (Message, callback) => {
-    console.log('createMessage', Message);
-    io.emit('newMessage', generateMessage(Message.from, Message.text))
-    callback('This is from the server');});
+    var user = users.getUser(socket.id);
+
+    if(user && isRealString(Message.text)){
+      io.to(user.room).emit('newMessage', generateMessage(user.name, Message.text))
+    }
+  });
+ 
+
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+
+    if(user){
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    }
   });
 
   socket.on('disconnect', () => {
